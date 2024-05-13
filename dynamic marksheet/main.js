83bc9e0bbd306737f0ee65efce1c5084da0608dc
Baeed khan <baeedk7@gmail.com>
2024-05-13 15:16:46 +0500

    dynamic marksheet

---
+++ dynamic marksheet/main.js
@@ -0,0 +1,18 @@
+let userName=prompt('what is your name')
+document.write(`<h1> ${userName} Marksheet</h1>`)
+
+
+document.write(`your name is:  ${userName}. <br>`)
+
+let schooName=prompt('what is your School name')
+document.write(`your School name is:  ${schooName}. <br>`)
+
+let totalMarks=prompt('Total marks')
+document.write(`your name is:  ${totalMarks}. <br>`)
+
+let obtainMarks=prompt(`how many numbers you've got.`)
+document.write(`obtain Marks: =  ${obtainMarks}. <br>`)
+
+let percantage = obtainMarks*100/totalMarks
+document.write(`your percantage: = ${percantage}%. <br>`)
+
