9f73b02f45cc037d38f1e2b552e047bafc4665de
Baeed khan <baeedk7@gmail.com>
2024-09-20 02:22:19 +0500

    'calculator'

--- calculator/script.js
+++ calculator/script.js
@@ -3,44 +3,48 @@ const button=document.getElementById('btn').addEventListener("click",function(){
 
     const inputElement1 = document.getElementById('input-1').value;
     const inputElement2 = document.getElementById('input-2').value;
-    let storedValue1 = parseFloat(inputElement1);
-    let storedValue2 = parseFloat(inputElement2);
+    const storedValue1 = parseFloat(inputElement1);
+    const storedValue2 = parseFloat(inputElement2);
 
    const select= document.getElementById('select')
    const selectValue =select.value;
    if(selectValue== '+'){
    const ans= storedValue1+storedValue2
    document.getElementById('output').textContent = ans
-    // inputElement1 = '';
-    // inputElement2 = '';
+
    }
    else if (selectValue=='-'){
     const ans=storedValue1-storedValue2
     document.getElementById('output').textContent = ans
-    // inputElement1 = '';
-    // inputElement2 = '';
+
    } 
    else if (selectValue=='*'){
     const ans=storedValue1*storedValue2
     document.getElementById('output').textContent = ans
-    // inputElement1 = '';
-    // inputElement2 = '';
+
    }
    else if(selectValue=='/'){
     const ans=storedValue1/ storedValue2
     document.getElementById('output').textContent = ans
-    // inputElement1 = '';
-    // inputElement2 = '';
+
    }
-//    else if (storedValue1===NaN){
-//     document.getElementById('output').textContent = "Please Enter The Number"
-// }
+//    else if(storedValue1==''){
+//     document.getElementById('output').textContent = "Invalid input"
+//    }
+
+
 else{
     document.getElementById('output').textContent = "InValid Input"
 }
 
-inputElement1.value= '';
-inputElement2.value = '';
+
     
 
+})
+
+const AC=document.getElementById('ac-btn').addEventListener('click',function(){
+    document.getElementById('input-1').value = '';
+    document.getElementById('input-2').value = '';
+    document.getElementById('output').textContent = '';
+    
 })
